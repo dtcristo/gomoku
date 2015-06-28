490abf65f286afca5b4fedf47b2f18c1a5d84b85
David Cristofaro <david@dtcristo.com>
2015-06-28 12:50:57 +1000

    Cleanup Utility module

--- lib/gomoku/utility.rb
+++ lib/gomoku/utility.rb
@@ -1,22 +1,24 @@
 # Helper methods
 module Utility
-  def self.r_to_y(r)
+  module_function
+
+  def r_to_y(r)
     r * 40 - 20
   end
 
-  def self.c_to_x(c)
+  def c_to_x(c)
     c * 40 - 20
   end
 
-  def self.x_to_c(x)
+  def x_to_c(x)
     (x.to_i + 20) / 40
   end
 
-  def self.y_to_r(y)
+  def y_to_r(y)
     (y.to_i + 20) / 40
   end
 
-  def self.in_range?(r, c)
+  def in_range?(r, c)
     r >= 1 && r <= 19 && c >= 1 && c <= 19
   end
 end
